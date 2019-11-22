e148d4d1e37420ad9bf3c18075d0b96b704f1216
AlekseyMikheev <dagas_88@mail.ru>
2019-11-22 20:14:15 +0300

    Update (add webpack ang other configurations, add components)

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-const path = require('path');
+const path = require(`path`);
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main.js',
+  mode: `development`,
+  entry: `./src/main.js`,
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, `public`),
+    filename: `bundle.js`
   },
-  devtool: 'source-map',
+  devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8081`,
+    compress: true,
+    watchContentBase: true
   }
 };
