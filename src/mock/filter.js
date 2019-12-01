80796889786d33a4807915269b1cfed59333b788
AlekseyMikheev <dagas_88@mail.ru>
2019-12-01 09:21:32 +0300

    Remove comments

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,7 +1,5 @@
 import {Filters} from '../const.js';
 
-// const FILTERS_COUNT = 10;
-
 const filterNames = [
   Filters.ALL, Filters.OVERDUE, Filters.TODAY, Filters.FAVORITES, Filters.REPEATING, Filters.TAGS, Filters.ARCHIVE
 ];
@@ -10,7 +8,7 @@ const generateFilters = () => {
   return filterNames.map((filter) => {
     return {
       name: filter,
-      count: 0// Math.floor(Math.random() * FILTERS_COUNT)
+      count: 0
     };
   });
 };
